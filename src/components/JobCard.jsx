0ac4432496bb0c4440d284297baae620b00133c1
BambamUncodemy <bambam@uncodemy.com>
2025-07-17 17:36:58 +0530

    Update JobCard.jsx

--- src/components/JobCard.jsx
+++ src/components/JobCard.jsx
@@ -1,12 +1,9 @@
 import react from 'react';
-
 function JobCard ({job}){
-    const jobTitle = job.job_title || job.title || 'Untitle';
-    const company = job.job_company_name || job.company || 'Unknow'
-    const logo = job.company_logo || 'Not Available'
-
+     const jobTitle = job.job_title || job.title || 'Untitled';
+  const company = job.employer_name || job.company?.display_name || 'Unknown';
+  const logo = job.employer_logo || job.company?.logo || 'https://via.placeholder.com/60';
     return(
-
     <div className="bg-white rounded-xl shadow-md p-5 border border-gray-200">
       <div className="flex items-start gap-4">
         <img src={logo} alt={company} className="w-14 h-14 object-contain rounded-lg border border-gray-200" />
@@ -18,5 +15,4 @@ function JobCard ({job}){
     </div>
 );
 }
-
 export default JobCard;
